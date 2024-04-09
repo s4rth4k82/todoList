f2d9a5b337095fd9f90f58a32b1bf6ecad1ce0d2
Sarthak Modhe <sarthakpmodhe0802@gmail.com>
2024-04-09 11:59:49 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is a an about component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque officiis inventore consectetur tenetur nobis delectus eveniet illo blanditiis fugiat hic doloremque, laborum iusto esse, rerum cum omnis eos repudiandae maxime!</p>
+        </div>
+    )
+}
